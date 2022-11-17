944baa4a489c216138edcf00743825ba3dfc332d
Ico Doornekamp <ico@zevv.nl>
2022-11-17 20:30:32 +0100

    cleanup

--- mover.nim
+++ mover.nim
@@ -7,16 +7,13 @@ export isIsolated
 type
   pthread_mutex_t {.importc: "pthread_mutex_t", pure, final, header: """#include <sys/types.h> include <pthread.h>""".} = object
   pthread_mutexattr_t* {.importc: "pthread_mutexattr_t", pure, final header: """#include <sys/types.h> include <pthread.h>""".} = object
+  pthread_cond_t {.importc: "pthread_cond_t", pure, final, header: """#include <sys/types.h> include <pthread.h>""".} = object
+  pthread_condattr_t {.importc: "pthread_condattr_t", pure, final header: """#include <sys/types.h> include <pthread.h>""".} = object
 
 proc pthread_mutex_init(L: var pthread_mutex_t, attr: ptr pthread_mutexattr_t) {. importc: "pthread_mutex_init", header: "<pthread.h>", noSideEffect.}
 proc pthread_mutex_lock(L: var pthread_mutex_t) {.noSideEffect, importc: "pthread_mutex_lock", header: "<pthread.h>".}
 proc pthread_mtuex_trylock(L: var pthread_mutex_t): cint {.noSideEffect, importc: "pthread_mutex_trylock", header: "<pthread.h>".}
 proc pthread_mutex_unlock(L: var pthread_mutex_t) {.noSideEffect, importc: "pthread_mutex_unlock", header: "<pthread.h>".}
-
-type
-  pthread_cond_t {.importc: "pthread_cond_t", pure, final, header: """#include <sys/types.h> include <pthread.h>""".} = object
-  pthread_condattr_t {.importc: "pthread_condattr_t", pure, final header: """#include <sys/types.h> include <pthread.h>""".} = object
-
 proc pthread_cond_init(cond: var pthread_cond_t, cond_attr: ptr pthread_condattr_t = nil) {. importc: "pthread_cond_init", header: "<pthread.h>", noSideEffect.}
 proc pthread_cond_wait(cond: var pthread_cond_t, lock: var pthread_mutex_t): cint {. importc: "pthread_cond_wait", header: "<pthread.h>", noSideEffect.}
 proc pthread_cond_signal(cond: var pthread_cond_t) {. importc: "pthread_cond_signal", header: "<pthread.h>", noSideEffect.} 
